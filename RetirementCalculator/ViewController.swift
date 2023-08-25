736793f986e38bab3e5d560971073ce52eaeed10
Newon-universe <chokovon@gmail.com>
2023-08-25 14:19:10 +0900

    Initial Commit

---
+++ RetirementCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetirementCalculator
+//
+//  Created by Kim Yewon on 2023/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
